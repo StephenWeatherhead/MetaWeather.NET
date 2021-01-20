158e21bd395b4bd460f4314d8b0cc7a6347e3ea2
Stephen Weatherhead <stephenw42@outlook.com>
2021-01-20 22:57:07 +0000

    Connect to an API and get JSON data as string

--- RetweetBot/Program.cs
+++ RetweetBot/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace RetweetBot
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "My Repository Reporter");
+            var myRepos = client.GetStringAsync("https://api.github.com/users/StephenWeatherhead/repos");
+            Console.WriteLine(await myRepos);
+            Console.ReadLine();
         }
     }
 }
