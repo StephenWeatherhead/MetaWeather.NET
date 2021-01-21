c4d6acc1f333e5db0f09d8e517a90a07130b1f48
Stephen Weatherhead <stephenw42@outlook.com>
2021-01-21 22:15:38 +0000

    LocationSearchQuery unit test

---
+++ MetaWeather.NET.Tests/MetaWeatherClientTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MetaWeather.NET.Tests
+{
+    public class MetaWeatherClientTests
+    {
+        [Fact]
+        public async Task LocationSearchQuery()
+        {
+            MetaWeatherClient client = new MetaWeatherClient();
+            LocationSearch locationSearch = await client.LocationSearch("london");
+            Assert.Equal("London", locationSearch.Title);
+            Assert.Equal("City", locationSearch.LocationType);
+            Assert.Equal(44418, locationSearch.WOEID);
+            Assert.Equal(51.51, Math.Round(locationSearch.Latitude, 2));
+            Assert.Equal(-0.13, Math.Round(locationSearch.Longitude, 2));
+        }
+    }
+}
---
+++ MetaWeather.NET/MetaWeatherClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaWeather.NET
+{
+    public class MetaWeatherClient : IMetaWeatherClient
+    {
+        public async Task<Location> Location(int WOEID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Forecast>> LocationDay(int WOEID, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LocationSearch> LocationSearch(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LocationSearch> LocationSearch(double latitude, double longitude)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
